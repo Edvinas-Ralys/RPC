5b0a91fa06e02ceb6693a78ca49f68e7c284e658
Edvinas-Ralys <edvinas.ralys97@gmail.com>
2023-09-26 21:54:36 +0300

    .

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,77 +1,164 @@
-//Prompt to ask for player input. Allows only valid answers
-// function playerInput(){
-//     let playerSelect = prompt(`Choose - rock, paper or scissors`).toLowerCase();
-//     if(playerSelect === `rock` || playerSelect === `paper` || playerSelect === `scissors` || playerSelect !== undefined){
-//         return playerSelect;
-//     }
-//     else{
-//         alert(`Wrong input, try again`);
-//         playerInput();
-//     }
-//     }
+        // adding buttons
 
-function displaySelection(selection){
-    const playerSelectBox = document.querySelector(`#playerSelectBox`);
-    playerSelectBox.innerHTML = selection;
-}
+//player buttons
+
+let player;
+let computer;
+
+const buttonRock = document.querySelector(`#rock`);
+buttonRock.addEventListener(`click`, rockFunction );
+
+const buttonPaper = document.querySelector(`#paper`);
+buttonPaper.addEventListener(`click`, paperFunction);
+
+const buttonScissors = document.querySelector(`#scissors`);
+buttonScissors.addEventListener(`click`, scissorsFunction)
+
+//computer buttons
+
+const buttonGenerate = document.querySelector(`#generate`);
+buttonGenerate.addEventListener(`click`, generateFunction);
+
+//play button
+
+const buttonPlay = document.querySelector(`#play`);
+buttonPlay.addEventListener(`click`, playGame);
+
+// reset button
+
+const resetButton = document.querySelector(`#reset`);
+resetButton.addEventListener(`click`, resetGame) 
 
 
+        //functions to display selections
 
+//player selection
 
+function rockFunction(){
+    document.getElementById(`playerSelectBox`).innerHTML = `rock`;
+    player = `rock`;
+};
+function paperFunction(){
+    document.getElementById(`playerSelectBox`).innerHTML = `paper`;
+    player = `paper`;
+};
+function scissorsFunction (){
+    document.getElementById(`playerSelectBox`).innerHTML = `scissors`;
+    player = `scissors`;
+};
 
+//computer selection
 
-
-
-
-
-
-
-
-
-
-//Computer choose what to play
-function computerInput(){
+function generateFunction(){
+    document.getElementById(`computerSelectBox`).innerHTML = `???`;
     let random = Math.random();
     if(random <=0.3333){
-        return `paper`;
+        computer = `paper`;
     }
     else if(random >=0.6666){
-        return `rock`;
+        computer = `rock`;
     }
     else{
-        return `scissors`;
+        computer = `scissors`;
     }
-}
-//Definig variables for game outcomes and scores
-let playerScore = 0;
-let computerScore = 0;
-let drawCount = 0
-let draw = `It's a draw!`
-let playerWin = `Player won this round!`
-let computerWin = `Computer won the round!`
+    console.log(computer);
+};
+            //game function
 
-//Plays a round of RPC
-function game(playerPlay, computerPlay){
-    playerPlay = playerInput();
-    console.log(`Player chose ` + playerPlay);
-    computerPlay = computerInput();
-    console.log(`Computer chose ` + computerPlay);
-  
-  if(playerPlay === computerPlay){
-    return draw;
-  }
-  else if (playerPlay === `rock` && computerPlay === `scissors`){
-    return playerWin;
-  }
-  else if (playerPlay === `paper` && computerPlay === `rock`){
-    return playerWin;
-}
-else if (playerPlay === `scissors` && computerPlay === `paper`){
-    return playerWin;
-}
-else{
-    return computerWin;
-}}
+let gameCount = document.getElementById(`gameCount`).innerHTML += 0;
+let playerScore = document.getElementById(`playerScore`);
+let computerScore = document.getElementById(`computerScore`);
+let drawGames = document.getElementById(`drawGames`);
+
+
+let draw = `Its a draw!`;
+let playerWin = `Player wins this round!`;
+let compWin = `Computer wins this round!`;
+let gameReset = ``;
+
+function playGame (){
+    buttonPlay.disabled = true;
+    buttonGenerate.disabled = true;
+    buttonPaper.disabled = true;
+    buttonRock.disabled = true;
+    buttonScissors.disabled = true;
+    document.getElementById(`computerSelectBox`).innerHTML = computer;
+    document.getElementById(`gameResult`).innerHTML
+    if(player === computer){
+        document.getElementById(`gameResult`).innerHTML = draw;
+        console.log(draw);
+        drawGames.innerHTML += 1;
+        return draw;
+    }
+    else if ((player === `rock` && computer === `scissors`)||
+            (player === `paper` && computer === `rock`)||
+            (player === `scissors` && computer === `paper`)){
+        console.log(playerWin);
+        document.getElementById(`gameResult`).innerHTML = playerWin;
+        playerScore.innerHTML +=1;
+        return playerWin;
+    }
+    else {
+        console.log(compWin)
+        document.getElementById(`gameResult`).innerHTML = compWin;
+        computerScore.innerHTML += 1;
+        return compWin;
+    }
+};
+
+// displying score
+
+
+function resetGame(){
+    buttonPlay.disabled = false;
+    buttonGenerate.disabled = false;
+    buttonPaper.disabled = false;
+    buttonRock.disabled = false;
+    buttonScissors.disabled = false;
+    document.getElementById(`gameResult`).innerHTML = gameReset;
+    document.getElementById(`computerSelectBox`).innerHTML = gameReset;
+    document.getElementById(`playerSelectBox`).innerHTML = gameReset;
+    
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //Plays 5 rounds. Displays score counter, announces round winner.
 //After 5 rounds displays winner of the game. End
