e54e84e6208d5efc7f1c0772d37f999b5238a699
Edvinas-Ralys <edvinas.ralys97@gmail.com>
2023-09-28 22:03:13 +0300

    .

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -48,7 +48,7 @@ function scissorsFunction (){
 };
 
 //computer selection
-
+buttonPlay.disabled = true;
 function generateFunction(){
     document.getElementById(`computerSelectBox`).innerHTML = `???`;
     let random = Math.random();
@@ -62,49 +62,71 @@ function generateFunction(){
         computer = `scissors`;
     }
     console.log(computer);
+    buttonPlay.disabled = false;
 };
             //game function
 
-let gameCount = document.getElementById(`gameCount`).innerHTML += 0;
+let gameCount = document.getElementById(`gameCount`);
 let playerScore = document.getElementById(`playerScore`);
 let computerScore = document.getElementById(`computerScore`);
 let drawGames = document.getElementById(`drawGames`);
-
+let playerSide = document.getElementById(`playerSide`)
+let computerSide = document.getElementById(`computerSide`);
 
 let draw = `Its a draw!`;
 let playerWin = `Player wins this round!`;
 let compWin = `Computer wins this round!`;
 let gameReset = ``;
+let i = 0;
+let j = 0;
+let a = 0;
+let b = 0;
 
-function playGame (){
+
+
+function playGame(){
     buttonPlay.disabled = true;
     buttonGenerate.disabled = true;
     buttonPaper.disabled = true;
     buttonRock.disabled = true;
     buttonScissors.disabled = true;
     document.getElementById(`computerSelectBox`).innerHTML = computer;
-    document.getElementById(`gameResult`).innerHTML
+    document.getElementById(`gameResult`).innerHTML;
+    gameCount.innerHTML = `Game games played: ` + ++i;
     if(player === computer){
         document.getElementById(`gameResult`).innerHTML = draw;
         console.log(draw);
-        drawGames.innerHTML += 1;
-        return draw;
+        drawGames.innerHTML = `Draws: ` + ++j;
+        draw;
     }
     else if ((player === `rock` && computer === `scissors`)||
             (player === `paper` && computer === `rock`)||
             (player === `scissors` && computer === `paper`)){
         console.log(playerWin);
         document.getElementById(`gameResult`).innerHTML = playerWin;
-        playerScore.innerHTML +=1;
-        return playerWin;
+        playerScore.innerHTML = `Player: ` + ++a;
+        playerWin;
     }
     else {
         console.log(compWin)
         document.getElementById(`gameResult`).innerHTML = compWin;
         computerScore.innerHTML += 1;
-        return compWin;
+        computerScore.innerHTML = `Computer: ` + ++b;
+        compWin;
+    };
+    if((i === 3) && (a > b)){
+        playerSide.style.cssText = `background-color: rgb(153, 255, 153);`
+        
     }
+    else if ((i === 3) && (a < b)) {
+        computerSide.style.cssText = `background-color: rgb(153, 255, 153);`
+        
+    };
+    if(i === 3){
+        gameCount.innerHTML = `Game games played: `
+    };
 };
+
 
 // displying score
 
@@ -118,7 +140,9 @@ function resetGame(){
     document.getElementById(`gameResult`).innerHTML = gameReset;
     document.getElementById(`computerSelectBox`).innerHTML = gameReset;
     document.getElementById(`playerSelectBox`).innerHTML = gameReset;
-    
+    buttonPlay.disabled = true;
+    playerSide.style.cssText = `background-color: rgb(204, 204, 255);`;
+    computerSide.style.cssText = `background-color: rgb(204, 204, 255);`;
 };
 
 
